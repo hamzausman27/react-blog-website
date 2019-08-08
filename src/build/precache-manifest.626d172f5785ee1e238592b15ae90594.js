5eab30ee1289391d9576e737dffdd691cf0e56ee
DESKTOP-V7EE2A4\mahru <hamza.usman27@gmail.com>
2019-08-09 04:54:40 +0500

    commit first

---
+++ src/build/precache-manifest.626d172f5785ee1e238592b15ae90594.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb1777703a9d9604073bcc5ec6c84ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a68c4c4ae7d979c2bc",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "bb660b66d97ca872da68",
+    "url": "/static/js/2.ad55af6c.chunk.js"
+  },
+  {
+    "revision": "63a68c4c4ae7d979c2bc",
+    "url": "/static/js/main.d14b6724.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
